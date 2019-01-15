d1838fc7d73b0b73bf1d5607998045eb69aac0cf
laishwaryat <46529112+laishwaryat@users.noreply.github.com>
2019-01-16 00:12:25 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,30 @@
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void func(int v, char s[]){
+
+if ((fork() == 0)) {
+	printf("%c",s[v]);
+	printf(" pid %d from  %d\n",getpid(),getppid());
+  if(v>0){
+   sleep(1);
+  func(v-1, s);
+  }
+}
+else {
+	wait (NULL) ;
+	
+}
+
+}
+
+int main()
+{
+char a[]="dlroW olleH";
+func(10 , a);
+return 0;
+}
+
