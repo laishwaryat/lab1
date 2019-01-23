3690dc46bc986ab74555d5bbeafab34f7de7d1b2
laishwaryat <46529112+laishwaryat@users.noreply.github.com>
2019-01-23 13:34:59 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ void func(int v, char s[]){
 
 if ((fork() == 0)) {
 	printf("%c",s[v]);
-	printf(" pid %d from  %d\n",getpid(),getppid());
+	printf(" pid\n",getpid());
   if(v>0){
    sleep(1);
   func(v-1, s);
